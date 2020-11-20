2bc2af0c4a4a8c52d25cc690359bb6e91a175fd9
Oliver K. Ernst <oernst@ucsd.edu>
2020-11-20 15:56:15 -0800

    added model numbers

--- scrape.py
+++ scrape.py
@@ -1,13 +1,32 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import pandas as pd
+from typing import List
+
+def remove_newlines(s : str) -> str:
+    return s.replace('\n',"").replace('\xa0',"")
+
+class Manufacturer:
+    def __init__(self, name : str):
+        self.name = remove_newlines(name)
+        self.models : List[str] = []
+
+    def set_models_from_comma_sep_str(self, full_str : str):
+        full_str = remove_newlines(full_str)
+        for s in full_str.split(','):
+            self.models.append(s)
+
+    def __repr__(self): 
+        return "%s: %s" % (self.name, ','.join(self.models))
+
+    def __str__(self):
+        return self.name
 
 url = "https://www.fda.gov/medical-devices/coronavirus-disease-2019-covid-19-emergency-use-authorizations-medical-devices/personal-protective-equipment-euas"
 page = urlopen(url)
 html_bytes = page.read()
 content = html_bytes.decode("utf-8")
 
-companies = []
+companies : List[Manufacturer] = []
 
 soup = BeautifulSoup(content)
 for h4 in soup.findAll('h4'):
@@ -23,10 +42,18 @@ for h4 in soup.findAll('h4'):
         # Iterate over table rows
         for tr in div.findAll("tr"):
 
-            # Iterate over cells
+            # First cell = manufacturer
             td = tr.find("td")
-
             if td != None:
-                companies.append(td.get_text())
+                company = Manufacturer(name=td.get_text())
 
-print(companies)
+                # Second cell = models, comma separated
+                td = td.find_next_sibling("td")
+                if td != None:
+                    company.set_models_from_comma_sep_str(td.get_text())
+
+                # Save company
+                companies.append(company)
+
+for c in companies:
+    print(repr(c))
